838cb9d0a4a67fdfd0eb3a0ff3506dc1741ebfd5
420kyle69 <66268622+420kyle69@users.noreply.github.com>
2023-06-03 19:44:50 -0700

    Add support for escaping new Discord markdown features: headings, bullet list, numbered list

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 module.exports = (text) => {
-    return text.replace(/(\_|\*|\~|\`|\||\\|\<|\>|\:|\!)/g, "\\$1").replace(/@(everyone|here|[!&]?[0-9]{17,21})/g, "@\u200b\$1");
+    return text.replace(/(\_|\*|\~|\`|\||\\|\<|\>|\:|\!|(?<=^|\n|\r)#+\s|(?<=(?:^|\n|\r)\s*)-\s)/g, "\\$1").replace(/((?<=(?:^|\n|\r)\s*)\d+)\.(\s)/g, "$1\\.$2").replace(/@(everyone|here|[!&]?[0-9]{17,21})/g, "@\u200b\$1");
 }
